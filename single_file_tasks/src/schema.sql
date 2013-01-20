adbfd75a4193eb923ed3db6caee536b4f2538c81
Tshepang Lekhonkhobe <tshepang@gmail.com>
2013-01-20 13:52:05 +0200

    counting starts from zero

--- single_file_tasks/src/schema.sql
+++ single_file_tasks/src/schema.sql
@@ -4,7 +4,7 @@ create table if not exists tasks (
     closed bool not null
 );
 
-insert or ignore into tasks (id, name, closed) values (1, 'Start learning Pyramid', 0);
-insert or ignore into tasks (id, name, closed) values (2, 'Do quick tutorial', 0);
-insert or ignore into tasks (id, name, closed) values (3, 'Have some beer!', 0);
+insert or ignore into tasks (id, name, closed) values (0, 'Start learning Pyramid', 0);
+insert or ignore into tasks (id, name, closed) values (1, 'Do quick tutorial', 0);
+insert or ignore into tasks (id, name, closed) values (2, 'Have some beer!', 0);
 
