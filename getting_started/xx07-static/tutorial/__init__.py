9cdf12080bba161d4c4f309321355be464f49fc5
Paul Everitt <paul@agendaless.com>
2013-03-07 11:56:23 -0500

    07 done.

--- getting_started/xx07-static/tutorial/__init__.py
+++
@@ -1,9 +0,0 @@
-from pyramid.config import Configurator
-
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.add_route('hello', '/')
-    config.add_static_view(name='static', path='tutorial:static')
-    config.scan()
-    return config.make_wsgi_app()
