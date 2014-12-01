d095c5108c6d7161bd58132ba4a86fa0f19e6033
patricklundquist <patricklundquist@gmail.com>
2014-11-30 17:49:58 -0800

    Added another missing fix for pyramid 1.5 requiring config.include('pyramid_chameleon') when using pt files Added missing packages to pyramid setup

--- getting_started/11-sqlalchemy/tutorial/tests.py
+++ getting_started/11-sqlalchemy/tutorial/tests.py
@@ -31,6 +31,7 @@ class WikiViewTests(unittest.TestCase):
 
     def test_wiki_view(self):
         from tutorial.views import WikiViews
+        self.config.include('pyramid_chameleon')
 
         request = testing.DummyRequest()
         inst = WikiViews(request)
