5cc07e3f9e7fbb55e7c36b97f0fe189df94513db
Steve Piercy <web@stevepiercy.com>
2017-06-09 14:12:50 -0700

    Restore html_use_smartypants option - RTD either does not pick up docutils.conf or is not supported in Sphinx 1.5.3

--- docs/conf.py
+++ docs/conf.py
@@ -152,6 +152,10 @@ html_title = 'The Pyramid Community Cookbook v%s' % release
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
 
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+html_use_smartypants = False # people use cutnpaste in some places
+
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
 
